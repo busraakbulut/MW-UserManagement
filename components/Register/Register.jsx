e1a809d237277c582bcf6d53accbf33b68c83022
Büşra <busraakbulutt45@gmail.com>
2023-06-04 04:33:48 +0300

    status update

--- components/Register/Register.jsx
+++ components/Register/Register.jsx
@@ -10,7 +10,7 @@ const Registration = () => {
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
   const [role, setRole] = useState("user");
-  const [status, setStatus] = useState("");
+  const [status, setStatus] = useState("inactive");
 
   async function getUsers() {
     const url = "http://localhost:3004/users";
@@ -118,11 +118,10 @@ const Registration = () => {
                   className="sr-only peer"
                   value={status}
                   onChange={(e) => setStatus(e.target.value)}
-                  required
                 />
                 <div className="w-11 h-6 bg-gray-200 rounded-2xl peer-focus:ring-4 peer-focus:ring-blue-300    peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
                 <span className="ml-3 text-sm font-medium text-gray-900">
-                  {status ? "Active" : "Inactive"}
+                  {status ? "Inactive" : "Active"}
                 </span>
               </label>
             </div>
