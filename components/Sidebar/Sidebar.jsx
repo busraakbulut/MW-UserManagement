c5f4c6cc22ff54dcf06e10af7dd51c89ac2e1786
Büşra <busraakbulutt45@gmail.com>
2023-06-05 20:45:49 +0300

    sidebar update

--- components/Sidebar/Sidebar.jsx
+++ components/Sidebar/Sidebar.jsx
@@ -1,7 +1,6 @@
 "use client";
-
 import React, { useState } from "react";
-import Card from "@material-tailwind/react";
+
 import {
   ChevronUpIcon,
   ChevronDownIcon,
@@ -18,13 +17,13 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="flex max-w-lg sm:w-1/4 lg:w-1/6 bg-white border border-gray-300 fixed h-screen top-14 left-0  shadow-sm shadow-blue-600">
+    <aside className=" max-w-lg sm:w-1/4 lg:w-1/6  fixed h-screen top-16 left-0  shadow-sm shadow-blue-600 bg-white border-r border-gray-300">
       <div>
         <button
           className="flex justify-between w-full p-4"
           onClick={toggleAccordion}
         >
-          <span className="font-medium ">User Management</span>
+          <span className="font-medium">User Management</span>
           {isOpen ? (
             <ChevronUpIcon className="h-5 w-5 text-gray-500" />
           ) : (
@@ -46,7 +45,6 @@ const Sidebar = () => {
           </div>
         )}
       </div>
-
       <div className="fixed bottom-0 left-0 p-4">
         <span className="flex py-2 items-center text-center">
           <UserCircleIcon className="h-8 w-8" />
@@ -61,7 +59,7 @@ const Sidebar = () => {
           Log Out
         </span>
       </div>
-    </div>
+    </aside>
   );
 };
 
